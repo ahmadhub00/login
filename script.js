d421cdab72cf8bc9bf6573f4e641533f35f2ccaa
Ahmad Taimur <ullahtaimur@gmail.com>
2025-04-22 23:51:43 +0500

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// DOM Elements
+const navMenu = document.getElementById('navMenu');
+const menuToggle = document.getElementById('menuToggle');
+const loginBtn = document.getElementById('loginBtn');
+const registerBtn = document.getElementById('registerBtn');
+const goToLogin = document.getElementById('goToLogin');
+const goToRegister = document.getElementById('goToRegister');
+const loginForm = document.getElementById('login');
+const registerForm = document.getElementById('register');
+
